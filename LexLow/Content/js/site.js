41035a4bf05fa0a89691de8a64105288e952a819
LexLow <aradzabov@gmail.com>
2014-03-12 01:28:37 +0500

    Navbar Top

---
+++ LexLow/Content/js/site.js
@@ -0,0 +1,14 @@
+﻿function NavbarLoc() {
+    var obj = $('#icon_navbar_lock')
+    var navbar = $('#navbar_main')
+
+    if (obj.attr('class') == '') {
+        obj.attr('class', 'glyphicon glyphicon-saved')
+        navbar.attr('class', 'navbar navbar-default navbar-fixed-top')
+    }
+    else {
+        obj.attr('class', '')
+        navbar.attr('class', 'navbar navbar-default')
+    }
+
+}
