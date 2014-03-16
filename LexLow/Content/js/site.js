20adf3b56be957a934cd30baf37c4d78782597a0
LexLow <aradzabov@gmail.com>
2014-03-16 23:39:44 +0500

    wysiwyg

--- LexLow/Content/js/site.js
+++ LexLow/Content/js/site.js
@@ -5,10 +5,19 @@
     if (obj.attr('class') == '') {
         obj.attr('class', 'glyphicon glyphicon-saved')
         navbar.attr('class', 'navbar navbar-default navbar-fixed-top')
+        $('#div_top_indent').attr('style','height:40px;')
     }
     else {
         obj.attr('class', '')
         navbar.attr('class', 'navbar navbar-default')
+        $('#div_top_indent').attr('style', '')
     }
+}
 
+function ChangeTheme(theme_file, icon_theme_name) {
+    $('#link_style').attr('href', theme_file)
+    $('#icon_theme_deffault').attr('class', '')
+    $('#icon_theme_light').attr('class', '')
+    $('#icon_theme_blue').attr('class', '')
+    $('#' + icon_theme_name).attr('class', 'glyphicon glyphicon-arrow-right')
 }
---
+++ LexLow/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using RTE;
+
+namespace rtedemo.Controllers
+{
+    public class BlogController : Controller
+    {
+
+
+        public ActionResult rte()
+        {
+            Editor Editor1 = new Editor(System.Web.HttpContext.Current, "Editor1");
+            Editor1.LoadFormData("Type Here...");
+            Editor1.Width = Unit.Percentage(100);
+            Editor1.Height = Unit.Pixel(500);
+            Editor1.Skin = "office2010black";
+             
+            Editor1.MvcInit();
+            ViewBag.Editor = Editor1.MvcGetString();
+            return View();
+        }
+
+    }
+}
